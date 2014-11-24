6eb8a6e5c33c20fd74b55dc9e744076041852d13
tibristo <timmybristowguy@gmail.com>
2014-11-24 16:29:27 +0000

    All of the added methods for qjets, fw20, softdrop and eec are working, not fully tested that the output makes sense though.  New qjets package is used, removed Sams.

--- Exotics/PlotTransverseMass/include/TLorentzVectorDict.h
+++ Exotics/PlotTransverseMass/include/TLorentzVectorDict.h
@@ -4,7 +4,10 @@
 #ifdef __CINT__
 #pragma link C++ class std::vector< TLorentzVector >;
 #pragma link C++ class std::vector< float >;
+#pragma link C++ class std::vector< std::vector< int > >;
 #endif
 
 template class std::vector< TLorentzVector >;
 template class std::vector< float >;
+template class std::vector< std::vector< int > >;
+
