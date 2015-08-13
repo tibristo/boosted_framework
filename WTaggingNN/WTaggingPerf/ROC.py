Trying to fix create_folds still. Right now I'm looking at how to get the scaling done.

WTaggingNN/WTaggingPerf/ROC.py
@@ -12,6 +12,7 @@ from ROOT import TH1F, TH2D, TCanvas, TFile, TNamed, gROOT
 from root_numpy import fill_hist
 import functions as fn
 gROOT.SetBatch(True)
+import os
 
 def data_load(filename):
     data = np.genfromtxt(filename, delimiter = ',', names = True)
@@ -332,7 +333,9 @@ def ROC_plotter(taggerdict, min_eff = 0, max_eff = 1, linewidth = 1.4, pp = Fals
                     
                     #matrix = np.vstack((data['efficiency'][sel],1-data['rejection'][sel])).T
                     #fill_hist(hist, matrix)
-                    fo = TFile.Open('ROC/AGILE_'+inputfile.replace('.pdf','')+'.root','RECREATE')
+                    if not os.path.exists('ROC_root'):
+                        os.makedirs('ROC_root')
+                    fo = TFile.Open('ROC_root/AGILE_'+inputfile.replace('.pdf','')+'.root','RECREATE')
                     data['roc_curve'].Write()
                     #hist.Write()
                     rej_str = 'rejection_power_'+str(rejpow)

WTaggingNN/create_folds.py
@@ -5,7 +5,7 @@
 # Learning curves
 # Probability distributions
 import numpy as np
-
+from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import ShuffleSplit, StratifiedKFold
 import os, sys, time
 import numpy.lib.recfunctions as nf
@@ -13,7 +13,7 @@ from pprint import pprint
 from root_numpy import array2root
 
 #import cv_fold
-def persist_cv_splits(X, y, n_cv_iter=5, test_size=0.25, name='data', prefix='folds/', suffix="_cv_%03d.root", random_state=None, scale=False):
+def persist_cv_splits(X, y, observers, n_cv_iter=5, test_size=0.25, name='data', prefix='folds/', suffix="_cv_%03d.root", random_state=None, scale=False):
     """Materialize randomized train test splits of a dataset."""
     cv = StratifiedKFold(y,n_folds=n_cv_iter,shuffle=True)
     #cv = ShuffleSplit(X.shape[0], n_iter=n_cv_iter,
@@ -23,31 +23,54 @@ def persist_cv_splits(X, y, n_cv_iter=5, test_size=0.25, name='data', prefix='fo
     
     for i, (train, test) in enumerate(cv):
         print ("iteration %03d" % i)
+        # can't scale all the variables - we don't want to scale weights!
         if scale:
             scaler = StandardScaler()
-            X_train_scaled = scaler.fit_transform(X[train])
-            X_test_scaled = scaler.transform(X[test])
+            #mean = X[train].mean()
+            #std = X[train].std()
+            #Xtrain = (X[train] - mean)/std
+            #Xtest = (X[test] - mean)/std
+            Xtrain = scaler.fit_transform(X[train])
+            Xtest = scaler.transform(X[test])
+        else:
+            Xtrain = X[train]
+            Xtest = X[test]
+        ytrain = y[train]
+        ytest = y[test]
         cv_split_filename = prefix+name + 'train' + suffix % i
         cv_split_train = os.path.abspath(cv_split_filename)
         cv_split_test = os.path.abspath(cv_split_filename.replace('train','test'))
 
         cv_split_filenames.append([cv_split_train, cv_split_test])
-        rectrain = nf.append_fields(X[train], names='label', data=y[train], usemask=False)#, dtypes=int)#, usemask=False)
+        xtrain_o = [Xtrain,observers[train]]
+        xtest_o = [Xtest,observers[test]]
+        merged_train = nf.merge_arrays(xtrain_o,flatten=True,usemask=True)
+        merged_test = nf.merge_arrays(xtest_o,flatten=True,usemask=True)
+        #print merged.shape
+        #print merged.dtype.names
+        rectrain = nf.append_fields(merged_train, names='label', data=ytrain, dtypes=np.int32, usemask=False)#, dtypes=int)#, usemask=False)
         #rec = nf.append_fields(X[train], 'label', y[train], usemask=False)
-        #array2root(rectrain, cv_split_train, 'outputTree', 'recreate')
-        rectest = nf.append_fields(X[test], names='label', data=y[test], usemask=False)#, dtypes=int)#, usemask=False)
-        #array2root(rectest, cv_split_test, 'outputTree', 'recreate')
+        array2root(rectrain, cv_split_train, 'outputTree', 'recreate')
+        rectest = nf.append_fields(merged_test, names='label', data=ytest, dtypes=np.int32,usemask=False)#, dtypes=int)#, usemask=False)
+        array2root(rectest, cv_split_test, 'outputTree', 'recreate')
     return cv_split_filenames
 
 
 def cross_validation(data,iterations, name='data'):
     variables = list(data.dtype.names)
     variables.remove('label')
+    # remove the variables that are "observers", ie that do not get used for training, or weights, since those must not be scaled.
+    observers = ['mc_event_weight','jet_antikt10truthtrimmedptfrac5smallr20_pt','jet_antikt10truthtrimmedptfrac5smallr20_eta','m','pt','eta','phi','evt_xsec','evt_filtereff','evt_nevts','weight','jet_camkt12truth_pt','jet_camkt12truth_eta','jet_camkt12truth_phi','jet_camkt12truth_m','jet_camkt12lctopo_pt','jet_camkt12lctopo_eta','jet_camkt12lctopo_phi','jet_camkt12lctopo_m']
+    for o in observers:
+        variables.remove(o)
     X = data[variables]
     y = data['label']
+    observer_data = data[observers]
+    #print observer_data.dtype.names
+    #raw_input()
     #w = data['weight']
 
-    filenames = persist_cv_splits(X, y, n_cv_iter=iterations, name=name, suffix="_cv_%03d.root", test_size=0.25, random_state=None)
+    filenames = persist_cv_splits(X, y, observer_data, n_cv_iter=iterations, name=name, suffix="_cv_%03d.root", test_size=0.25, random_state=None, scale=False)
     #all_parameters, all_tasks = grid_search(
      #   lb_view, model, filenames, params)
     return filenames
@@ -55,7 +78,7 @@ def cross_validation(data,iterations, name='data'):
 
 def plotFiles(filenames, variables):
     import ROOT
-    ROOT.SetBatch(True)
+    ROOT.gROOT.SetBatch(True)
     print filenames
     for f in filenames:
         print f
@@ -105,15 +128,16 @@ def plotFiles(filenames, variables):
 print os.getcwd()
 from collections import OrderedDict
 
-
-algorithm = 'AntiKt10LCTopoTrimmedPtFrac5SmallR20_13tev_matchedL_ranged_v2_1000_1500_nomw'
-name = sys.argv[1].replace('.csv','')
-if name.find('/')!=-1:
-    name = name[name.rfind('/')+1:]
-print name
-
+path = '/Disk/ecdf-nfs-ppe/atlas/users/tibristo/BosonTagging/csv/'
+algorithm = 'AntiKt10LCTopoTrimmedPtFrac5SmallR20_13tev_matchedM_loose_v2_500_1000_nomw_merged'
+#name = sys.argv[1].replace('.csv','')
+#if name.find('/')!=-1:
+#    name = name[name.rfind('/')+1:]
+#print name
+name = algorithm
 cols = np.linspace(1,43,43,dtype=int)
-data = np.recfromcsv(sys.argv[1],usecols=cols)
+#data = np.recfromcsv(sys.argv[1],usecols=cols)
+data = np.recfromcsv(path+algorithm+'.csv',usecols=cols)
 
 filenames = cross_validation(data, 4, name)
 